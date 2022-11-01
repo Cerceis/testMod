717a24e6be5a3d920db946697063264bcec225eb
Cerceis <48720021+Cerceis@users.noreply.github.com>
2022-11-01 17:28:53 +0900

    1.0.2

--- src/Abilities.js
+++ src/Abilities.js
@@ -11,7 +11,7 @@ export const Abilities = {
         id: "0000",
         name: "Extra physical hit",
         source: SOURCE,
-        description: "At the start of every turn, you dealt 1 extra physical attack.",
+        description: "At the start of every turn, you dealt 1 extra physical attack and increase your physical defense by 5.",
         stackable: true,
         effects:{
             "onTurnStart":[
@@ -19,6 +19,7 @@ export const Abilities = {
                     if(!enemy || !game) return;
                     await game.addLog(`${player.name} dealing one extra physical attack!`)
                     await player.physicalAttack(enemy);
+                    player.modPhysicalDef += 5;
                 }
             ]
         }
